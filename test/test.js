82ca68da563306d4267b42f36aff13db23aab36d
ryo0301 <ryo0301@gmail.com>
2016-10-02 17:37:52 +0900

    modify test

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@
 const expect = require('chai').expect;
 const TickerStream = require('../lib/TickerStream.js');
 
-describe('Constructor', () => {
-  it('should have default property value', () => {
+describe('Constructor', function() {
+  it('should have default property value', function() {
     const ts = new TickerStream();
     expect(ts._tickerState.min).to.equal(100);
     expect(ts._tickerState.max).to.equal(1000);
@@ -13,7 +13,7 @@ describe('Constructor', () => {
     expect(ts._tickerState.phony()).to.be.a('object');
   });
 
-  it('should have specified property value', () => {
+  it('should have specified property value', function() {
     const ts = new TickerStream({
       min: 1,
       max: 10,
@@ -27,7 +27,7 @@ describe('Constructor', () => {
     expect(ts._tickerState.phony()).to.deep.equal({k:'v'});
   });
 
-  it('should swap min and max if min larger than max', () => {
+  it('should swap min and max if min larger than max', function() {
     const min = 1000;
     const max = 10;
     const ts = new TickerStream({ min, max });
@@ -35,34 +35,35 @@ describe('Constructor', () => {
     expect(ts._tickerState.max).to.equal(min);
   });
 
-  it('should not start until resume event emitted', () => {
+  it('should not start until resume event emitted', function() {
     const ts = new TickerStream();
     expect(ts._tickerState.ticks).to.have.length(0);
   });
 });
 
-describe('Events', () => {
-  it('should handle data event', done => {
+describe('Events', function() {
+  it('should handle data event', function(done) {
     const ts = new TickerStream({
       duration: 100,
       phony: () => ({k:'v'})
     });
-    ts.on('data', (data) => {
+    ts.on('data', function(data) {
       expect(data).to.deep.equal({k:'v'});
       done();
     });
   });
 
-  it('should not handle readable event', done => {
+  it('should not handle readable event', function(done) {
+    this.slow(250);
     const ts = new TickerStream({
       min: 5,
       max: 5,
       duration: 100
     });
-    ts.on('readable', () => {
+    ts.on('readable', function() {
       done(new Error('Handle readable event'));
     });
-    setTimeout(() => {
+    setTimeout(function() {
       done();
     }, 200);
   });
